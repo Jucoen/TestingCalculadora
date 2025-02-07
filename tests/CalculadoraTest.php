c63ce36abf714afb4f0c6a95b709e1d5a7cd7458
Jucoen <jucoen@alumnatflorida.es>
2025-02-07 22:01:16 +0100

    cambiando testeo

--- tests/CalculadoraTest.php
+++ tests/CalculadoraTest.php
@@ -5,39 +5,39 @@
 
         public function testSuma() { 
             $calc = new Calculadora(); 
-            $resultado = $calc -> suma(3, 2); 
-            $this -> assertEquals(5, $resultado); 
-            $this -> assertSame(5, $resultado); 
-            $this -> assertGreaterThan(4, $resultado); 
+            $resultado = $calc -> suma(4, 2); 
+            $this -> assertEquals(6, $resultado); 
+            $this -> assertSame(6, $resultado); 
+            $this -> assertGreaterThan(5, $resultado); 
             $this -> assertNotNull($resultado); 
         } 
         public function testResta() { 
             $calc = new Calculadora(); 
-            $resultado = $calc -> resta(3, 2); 
-            $this -> assertEquals(1, $resultado); 
-            $this -> assertGreaterThan(0, $resultado); 
+            $resultado = $calc -> resta(4, 2); 
+            $this -> assertEquals(2, $resultado); 
+            $this -> assertGreaterThan(1, $resultado); 
             $this -> assertNotNull($resultado); 
         } 
         public function testMultiplicacion() { 
             $calc = new Calculadora(); 
-            $resultado = $calc -> multiplicacion(3, 2); 
-            $this -> assertEquals(6, $resultado); 
-            $this -> assertGreaterThan(0, $resultado); 
+            $resultado = $calc -> multiplicacion(4, 5); 
+            $this -> assertEquals(20, $resultado); 
+            $this -> assertGreaterThan(19, $resultado); 
             $this -> assertNotNull($resultado); 
         } 
         public function testDivision() { 
             $calc = new Calculadora(); 
-            $resultado = $calc -> division(4, 2); 
-            $this -> assertEquals(2, $resultado); 
-            $this -> assertGreaterThan(0, $resultado); 
+            $resultado = $calc -> division(8, 2); 
+            $this -> assertEquals(4, $resultado); 
+            $this -> assertGreaterThan(3, $resultado); 
             $this -> assertNotNull($resultado); 
         }
 
     public function testRaiz() { 
         $calc = new Calculadora(); 
-        $resultado = $calc -> raiz(4); 
-        $this -> assertEquals(2, $resultado); 
-        $this -> assertGreaterThan(0, $resultado); 
+        $resultado = $calc -> raiz(16); 
+        $this -> assertEquals(4, $resultado); 
+        $this -> assertGreaterThan(3, $resultado); 
         $this -> assertNotNull($resultado); 
     }
  }
